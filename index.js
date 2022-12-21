865f7f52e0c6565d449b81d4f0d6ad89269d5e3c
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-12-21 20:52:42 +0600

    finish

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-
+// middle wares
 app.use(cors());
 app.use(express.json());
 
